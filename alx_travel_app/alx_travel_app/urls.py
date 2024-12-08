ba196c1d9e2692e6189c81dc6e46c65835d698e3
Mikelesnr <mwanza.n.m@gmail.com>
2024-12-08 22:21:41 +0200

    urls.py edit and db migration

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
